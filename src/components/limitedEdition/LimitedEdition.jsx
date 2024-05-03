9f4d6eb384f20c3caebe5c0c9c15155ace5f76a5
santiago.aguirre@uosolutions.com <santiago.aguirre@uosolutions.com>
2024-05-03 18:24:30 -0300

    ajustes menores

--- src/components/limitedEdition/LimitedEdition.jsx
+++ src/components/limitedEdition/LimitedEdition.jsx
@@ -8,7 +8,7 @@ const LimitedEdition = () => {
     <div className="relative overflow-x-hidden bg-[#FFDA03] py-20">
       <div className="absolute inset-0 z-0 overflow-y-hidden">
         <Image src={lemonnadeBg} alt="background image"/>
-        <div class="absolute inset-0 bg-[#FFDA03] opacity-70 z-10"></div>
+        <div className="absolute inset-0 bg-[#FFDA03] opacity-70 z-10"></div>
       </div>
       <div className="z-50 relative  text-center flex flex-col items-center">
       <section className="font-[Barlow-700]">
